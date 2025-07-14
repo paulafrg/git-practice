657084252a7f0bbba001cce78f65ca5e9d2073b1
Paula Galvao <51344171+paulafrg@users.noreply.github.com>
2025-07-14 13:58:50 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+alert('Welcome to our Secret Number game!');
+let maxNumber = 5000;
+let secretNumber = parseInt(Math.random() * maxNumber+1);
+console.log(secretNumber);
+let guess;
+let tries = 1;
+
+// enquanto / while guessed number is different to the s.n.
+while(guess !=secretNumber) {
+    guess = prompt(`Choose a number between 1 and ${maxNumber}.`);
+    console.log(`Guessed number: ${guess}`);
+    console.log(`Compared results: ${guess == secretNumber}`);
+    // if the guess is the equal to the secret number
+    if (guess == secretNumber) {
+        break;
+    } else {
+        if(guess > secretNumber) {
+            console.log(`User guessed a bigger number, try again`);
+            alert(`The Secret Number is smaller than ${guess}`);
+        } else {
+            console.log(`User guessed a smaller number, try again`);
+            alert(`The Secret number is bigger than ${guess}`);
+        }
+        //tries = tries + 1;
+        tries++
+    }
+}
+
+let wordTry = tries > 1 ? 'tries': 'try';
+alert(`Congrats! You discovered the Secret Number (${secretNumber}) in ${tries} ${wordTry}.`);
+//if (tries > 1) {
+//    alert(`Congrats! You discovered the Secret Number (${secretNumber}) in ${tries} tries.`);
+//        console.log('The player was sucessful, rewards added');
+//} else {
+//alert(`Congrats! You discovered the Secret Number (${secretNumber}) in ${tries} try.`);
+//        console.log('The player was sucessful, rewards added');
+//}
